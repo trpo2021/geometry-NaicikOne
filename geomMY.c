13237ca9c3d2bdd10f8bf4d5466238b15cbacc10
NaicikOne <NaicikOne@yandex.ru>
2021-03-25 22:40:41 +0600

    final version

--- geomMY.c
+++ geomMY.c
@@ -5,149 +5,157 @@
 #include <stdlib.h>
 #include <string.h>
 
-bool search_brecket(char *MARK);
-bool search_comma(char *MARK);
-bool search_brecket2(char *MARK);
-bool word_chek(char *WR, char *MARK);
-int chek(char *MARK, int max, char *WR);
-void calculation(double *);
-void translate(char *MARK, double *D);
+bool search_brecket(char*);
+bool search_comma(char*);
+bool search_brecket2(char*);
+bool word_chek(char*, char*);
+int chek(char*, char*);
+void calculation(double*);
+void translate(char*, double*);
 
-int main() {
-  int max = 30;
-  char str[max];
-  char *MARK = str;
-  char *MARK2 = str;
-  double D[5];
-  char WR[] = {"circle"};
+int main()
+{
+    int max = 30;
+    char str[max];
+    char* MARK = str;
+    char* MARK2 = str;
+    double D[5];
+    char WR[] = {"circle"};
 
-  fgets(str, max, stdin);
+    fgets(str, max, stdin);
 
-  if (isalpha(*MARK) != 0) {
-    while (isalpha(*MARK2) != 0)
-      MARK2++;
-  } //передвижение указателя на первую скобку
+    if (isalpha(*MARK) != 0) {
+        while (isalpha(*MARK2) != 0)
+            MARK2++;
+    } //передвижение указателя на первую скобку
 
-  if (chek(MARK, max, WR) == 0) {
-    MARK = MARK2;
-    printf("Correct\n");
-  } else {
-    printf("Uncorrect\n");
-    system("pause");
-    return 0;
-  } // вывод сообщения о корректности запроса
+    if (chek(MARK, WR) == 0) {
+        MARK = MARK2;
+        printf("Correct\n");
+    } else {
+        printf("Uncorrect\n");
+        system("pause");
+        return 0;
+    } // вывод сообщения о корректности запроса
 
-  translate(MARK, D);
-  calculation(D);
+    translate(MARK, D);
+    calculation(D);
 
-  return true;
+    return true;
 }
 
-void translate(char *MARK, double *X) {
-  char *flag;
-  double a;
-  int i = 0;
-  while (isdigit(*MARK) == 0) {
-    MARK++;
-    if (isdigit(*MARK) != 0) {
-      a = strtod(MARK, &flag);
-      X[i] = a;
-      i++;
-      MARK = flag;
-      if (i == 3)
-        break;
-    } else {
-      printf("expected '<double>'\n");
-      system("pause");
-      break;
+void translate(char* MARK, double* X)
+{
+    char* flag;
+    double a;
+    int i = 0;
+    while (isdigit(*MARK) == 0) {
+        MARK++;
+        if (isdigit(*MARK) != 0) {
+            a = strtod(MARK, &flag);
+            X[i] = a;
+            i++;
+            MARK = flag;
+            if (i == 3)
+                break;
+        } else {
+            printf("expected '<double>'\n");
+            system("pause");
+            break;
+        }
     }
-  }
 } //преобразование чисел в строке в численный формат данных
 
-void calculation(double *D) {
-  float pi = 3.1415;
-  float S;
-  float P;
-  P = 2 * pi * D[2];
-  S = pi * pow(D[2], 2);
-  printf("Perimetr = %f\n", P);
-  printf("Area = %f\n", S);
-  system("pause");
+void calculation(double* D)
+{
+    float pi = 3.1415;
+    float S;
+    float P;
+    P = 2 * pi * D[2];
+    S = pi * pow(D[2], 2);
+    printf("Perimetr = %f\n", P);
+    printf("Area = %f\n", S);
+    system("pause");
 } // нахождение периметра и площади
 
-int chek(char *MARK, int max, char *WR) {
-  int res = 0;
-  if (search_brecket(MARK) == 0) { //поиск '('
-    res++;
-  }
-  if (search_brecket2(MARK) == 0) { //поиск ')'
-    res++;
-  }
-  if (search_comma(MARK) == 0) { //поиск ','
-    res++;
-  }
-  if (word_chek(MARK, WR) == 0) {
-    res++;
-  }
-  return res;
+int chek(char* MARK, char* WR)
+{
+    int res = 0;
+    if (search_brecket(MARK) == 0) { //поиск '('
+        res++;
+    }
+    if (search_brecket2(MARK) == 0) { //поиск ')'
+        res++;
+    }
+    if (search_comma(MARK) == 0) { //поиск ','
+        res++;
+    }
+    if (word_chek(MARK, WR) == 0) {
+        res++;
+    }
+    return res;
 } //массовая проверка верности запроса
 
-bool search_brecket(char *MARK) {
-  int flag = 0;
+bool search_brecket(char* MARK)
+{
+    int flag = 0;
 
-  while (*MARK != 10) {
-    if (*MARK == '(') {
-      flag = 1;
-      break;
+    while (*MARK != 10) {
+        if (*MARK == '(') {
+            flag = 1;
+            break;
+        }
+        MARK++;
     }
-    MARK++;
-  }
-  if (flag == 0) {
-    printf("expected '('\n");
-    return false;
-  }
-  return true;
+    if (flag == 0) {
+        printf("expected '('\n");
+        return false;
+    }
+    return true;
 } //проверка на наличие "("
 
-bool search_brecket2(char *MARK) {
-  int flag = 0;
+bool search_brecket2(char* MARK)
+{
+    int flag = 0;
 
-  while (*MARK != 10) {
-    if (*MARK == ')') {
-      flag = 1;
-      break;
+    while (*MARK != 10) {
+        if (*MARK == ')') {
+            flag = 1;
+            break;
+        }
+        MARK++;
     }
-    MARK++;
-  }
-  if (flag == 0) {
-    printf("expected ')'\n");
-    return false;
-  }
-  return true;
+    if (flag == 0) {
+        printf("expected ')'\n");
+        return false;
+    }
+    return true;
 } //проверка на наличие ")"
 
-bool search_comma(char *MARK) {
-  int flag = 0;
+bool search_comma(char* MARK)
+{
+    int flag = 0;
 
-  while (*MARK != 10) {
-    if (*MARK == ',') {
-      flag = 1;
-      break;
+    while (*MARK != 10) {
+        if (*MARK == ',') {
+            flag = 1;
+            break;
+        }
+        MARK++;
     }
-    MARK++;
-  }
-  if (flag == 0) {
-    printf("expected ','\n");
-    return false;
-  }
-  return true;
+    if (flag == 0) {
+        printf("expected ','\n");
+        return false;
+    }
+    return true;
 } //проверка на наличие ","
 
-bool word_chek(char *WR, char *MARK) {
-  int s = 6;
-  if (strncmp(WR, MARK, s) == 0) {
-    return true;
-  }
-  printf("check the spelling of the word\n");
-  return false;
+bool word_chek(char* WR, char* MARK)
+{
+    int s = 6;
+    if (strncmp(WR, MARK, s) == 0) {
+        return true;
+    }
+    printf("check the spelling of the word\n");
+    return false;
 } //проверка на написание слова "circle"
